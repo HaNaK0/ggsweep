22595b470e787e0573a047bdc3a3fa34cda8f335
HaNaK0 <saturn-v@hotmail.com>
2021-01-03 23:04:31 +0100

    Added simple logger for logging

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use ggez::{event, ContextBuilder, GameResult, graphics};
+use log::{info};
 
 
 mod states;
 use states::MainState;
 
 fn main() -> GameResult {
+	simple_logger::SimpleLogger::new()
+		.with_level(log::LevelFilter::Warn)
+		.with_module_level("ggsweep", log::LevelFilter::Trace)
+		.init()
+		.unwrap();
+
 	let cb = ContextBuilder::new("Mine Sweeper", "HaNaK0");
 
 	let (ctx, events_loop) = &mut cb.build()?;
 
-	println!("{}", graphics::renderer_info(ctx)?);
+	info!("{}", graphics::renderer_info(ctx)?);
 	let state = &mut MainState::new()?;
 
 	event::run(ctx, events_loop, state)

5a64aeec23c82b74882e74470c521ac227bc0a77
HaNaK0 <saturn-v@hotmail.com>
2021-05-08 16:26:29 +0200

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ggez::{ContextBuilder, conf::WindowMode, event, graphics};
 use ggsweep::{config::GameConfig, err_here, error::LocatedError, states::GameState};
 use log::info;
 
-use ggsweep::states::{MainState};
+use ggsweep::states::MainState;
 
 fn main() -> Result<(), LocatedError> {
     // Start the logger
@@ -26,35 +26,40 @@ fn main() -> Result<(), LocatedError> {
         path::PathBuf::from("./resources")
     };
 
+    // Create the context builder
     let cb = ContextBuilder::new("Mine Sweeper", "HaNaK0")
         .add_resource_path(resource_dir);
 
+    // Build the context
     let (ctx, events_loop) = &mut cb.build().map_err(err_here!())?;
 
     info!("{}", graphics::renderer_info(ctx).map_err(err_here!())?);
 
+    //Load config
     let game_config_file = ggez::filesystem::open(ctx, "\\config.ron").map_err(err_here!())?;
     let game_config: GameConfig = ron::de::from_reader(game_config_file).map_err(err_here!())?;
 
+    //Set window mode
     let mode = WindowMode::default().dimensions(
         game_config.game_size.0 as f32 * game_config.square_size, 
         game_config.game_size.1 as f32 * game_config.square_size
     );
-
     graphics::set_mode(ctx, mode).map_err(err_here!())?;
 
+    //Set the screen coordinates
     let screen_rect = graphics::Rect::new(
         0.0, 
         0.0, 
         game_config.game_size.0 as f32 * game_config.square_size, 
         game_config.game_size.1 as f32 * game_config.square_size
     );
-
     graphics::set_screen_coordinates(ctx, screen_rect).map_err(err_here!())?;
 
+    // Set the initial state
     let initial_state = Box::new(GameState::new(ctx, game_config).map_err(err_here!())?);
     let state = &mut MainState::new(initial_state, graphics::Color::from_rgb(38, 38, 38))
         .map_err(err_here!())?;
 
+    // Run
     event::run(ctx, events_loop, state).map_err(err_here!())
 }
